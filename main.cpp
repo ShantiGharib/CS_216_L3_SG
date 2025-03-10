f1cd1d20e761ee657a7ed2ed49e527e25c973621
ShantiGharib <gharibsj2020@student.laccd.edu>
2025-03-09 18:56:14 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -69,8 +69,7 @@ void HandleDefaultMenuCase();
 
 static constexpr const int CREATURE_SIZE_MAX{25}, HEADING_WIDTH{65};
 
-//static const std::string INPUT_FILE{"in_creatures.txt"};
-static const std::string INPUT_FILE{"C:\\Users\\Shanti\\Desktop\\New Text Document.txt"};
+static const std::string INPUT_FILE{"in_creatures.txt"};
 
 
 int main() {
